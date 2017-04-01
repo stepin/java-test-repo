89a3b6643b1188a750a25933bbc3c3ead2c8a997
Igor Stepin <stepin@MacBook-Pro-Igor.local>
2017-04-02 01:12:46 +0400

    More test variables

--- src/main/java/name/stepin/Main.java
+++ src/main/java/name/stepin/Main.java
@@ -4,6 +4,11 @@ import name.stepin.SampleClass.UsedInnerClass;
 
 public class Main {
 
+  public static int unusedPublic1 = 1;
+  protected int unusedPublic2 = 2;
+  static int unusedPublic3 = 3;
+  int unusedPublic4 = 4;
+
   public static void main(String[] args) {
     SampleClass sampleClass = new SampleClass();
     String s = sampleClass.usedMethod();
